732092d44dfbbd8a0c8aebe91a5893f8d0a5c5ce
Markus Herrmannsdoerfer <herrmama@in.tum.de>
2011-05-11 06:19:07 +0000

    [345336] TTC GMF example
    Former-commit-id: 5926709bebcb2ae7170dc4c4c46eba53da4e9829

---
+++ examples/ttc_gmf/src/GMFGraphTypedFigureCustomMigration.java
@@ -0,0 +1,19 @@
+import org.eclipse.emf.edapt.migration.CustomMigration;
+import org.eclipse.emf.edapt.migration.Instance;
+import org.eclipse.emf.edapt.migration.Metamodel;
+import org.eclipse.emf.edapt.migration.Model;
+
+public class GMFGraphTypedFigureCustomMigration extends CustomMigration {
+
+	@Override
+	public void migrateAfter(Model model, Metamodel metamodel) {
+		for(Instance fa : model.getAllInstances("gmfgraph.FigureAccessor")) {
+			Instance tf = fa.getLink("typedFigure");
+			if(tf == null) {
+				tf = model.newInstance("gmfgraph.CustomFigure");
+				tf.set("qualifiedClassName", "org.eclipse.draw2d.IFigure");
+				fa.set("typedFigure", tf);
+			}
+		}
+	}
+}
