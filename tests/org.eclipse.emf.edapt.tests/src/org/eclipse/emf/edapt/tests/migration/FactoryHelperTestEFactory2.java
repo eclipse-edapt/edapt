714066f0841f83f4f41633158b91d4fb1d0a717a
Alexandra Buzila <abuzila@eclipsesource.com>
2016-05-19 16:07:18 +0200

    Bug 474577 - Register migration factory for all versions using wildcards
    Modified *emf.edapt.factories extension point to consider wildcards in
    the nsURI during the factory registration.
    
    Change-Id: I9edb1e1c86a69de747487b86a49b0aab3e92e644
    Signed-off-by: Alexandra Buzila <abuzila@eclipsesource.com>
    [implementation]
    Signed-off-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>
    [test integration in build]
    Former-commit-id: 7ccb7ec17767773709676ac3f18d8193c5745cef

---
+++ tests/org.eclipse.emf.edapt.tests/src/org/eclipse/emf/edapt/tests/migration/FactoryHelperTestEFactory2.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2016 BMW Car IT, TUM, EclipseSource Muenchen GmbH, and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Alexandra Buzila - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.edapt.tests.migration;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+/**
+ * Test EFactory for the FactoryHelperTest.
+ *
+ * @author Alexandra Buzila
+ *
+ */
+public class FactoryHelperTestEFactory2 extends EFactoryImpl {
+
+}
