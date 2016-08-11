46bde2e861c710a8db0b29347e17f8769e0a4d89
J. Langley <jlangley@cohesionforce.com>
2016-08-11 11:38:31 -0500

    Bug 499473 - Delete Class violates 'validType' constraint
    Updated DeleteClass operation to remove instances of deleted classes,
    and remove empty resources from the model resource set. Also included a
    Junit testcase that runs with the SimpleExamplesLifecycleTest.
    
    Change-Id: Iba8fc095883d7eaae8eed7f801ef01e02639e885
    Signed-off-by: J. Langley <jlangley@cohesionforce.com>
    
    Former-commit-id: d19968d2e9fc3655fbc78c4189dc2ce6e3850463

--- plugins/org.eclipse.emf.edapt.declaration/src/org/eclipse/emf/edapt/declaration/creation/DeleteClass.java
+++ plugins/org.eclipse.emf.edapt.declaration/src/org/eclipse/emf/edapt/declaration/creation/DeleteClass.java
@@ -6,8 +6,10 @@ import org.eclipse.emf.edapt.declaration.EdaptConstraint;
 import org.eclipse.emf.edapt.declaration.EdaptOperation;
 import org.eclipse.emf.edapt.declaration.EdaptParameter;
 import org.eclipse.emf.edapt.declaration.OperationImplementation;
+import org.eclipse.emf.edapt.spi.migration.Instance;
 import org.eclipse.emf.edapt.spi.migration.Metamodel;
 import org.eclipse.emf.edapt.spi.migration.Model;
+import org.eclipse.emf.edapt.spi.migration.ModelResource;
 
 /**
  * {@description}
@@ -48,5 +50,22 @@ public class DeleteClass extends OperationImplementation {
 	public void execute(Metamodel metamodel, Model model) {
 		// metamodel adaptation
 		metamodel.delete(eClass);
+
+		// Now delete any instance of the eClass
+		for (final Instance instance : model.getAllInstances(eClass)) {
+
+			final ModelResource resource = instance.getResource();
+
+			// Remove the instance from its containing resource
+			resource.getRootInstances().remove(instance);
+
+			// If the resource is now empty, remove it from the model resources
+			if (resource.getRootInstances().size() == 0) {
+				model.getResources().remove(resource);
+			}
+
+			// Delete the instance of the deleted eClass
+			model.delete(instance);
+		}
 	}
 }
